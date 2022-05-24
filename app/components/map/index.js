fac1c8b7756548ede6e207cb7721e93d05a347a7
Rub21 <ruben@developmentseed.org>
2022-05-24 15:58:37 -0500

    Get geo coordinates fron magic wand

--- app/components/map/index.js
+++ app/components/map/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useLayoutEffect } from 'react';
 import T from 'prop-types';
 import Map from 'ol/Map';
 import View from 'ol/View';
@@ -11,16 +11,16 @@ import 'ol/ol.css';
 import { osm, vector, mainLayer } from './layers';
 import { MapContext } from '../../contexts/MapContext';
 import { ProjectLayer } from './ProjectLayer';
+import { Example } from '../test';
 
 export function MapWrapper({ project, children }) {
   const [map, setMap] = useState();
-
   const mapElement = useRef();
   const mapRef = useRef();
   mapRef.current = map;
-
-  useEffect(() => {
-    const wand = new MagicWandInteraction({
+  const [wand, setWand] = useState(null);
+  useLayoutEffect(() => {
+    const initWand = new MagicWandInteraction({
       layers: [mainLayer],
       hatchLength: 4,
       hatchTimeout: 300,
@@ -36,22 +36,49 @@ export function MapWrapper({ project, children }) {
     const initialMap = new Map({
       target: mapElement.current,
       controls: defaultControls().extend([new FullScreen()]),
-      interactions: defaultInteractions().extend([wand]),
+      interactions: defaultInteractions().extend([initWand]),
       layers: [osm, mainLayer, vector],
       view: view
     });
 
     setMap(initialMap);
+    setWand(initWand);
   }, []);
+
+  // useEffect(() => {
+
+  // }, []);
+
+  const handleClick = (e) => {
+    if (e.type === 'contextmenu') {
+      getContoursValues();
+    }
+  };
+
+  const getContoursValues = () => {
+    if (wand) {
+      let contours = wand.getContours();
+      if (!contours) return;
+      let rings = contours.map((c) =>
+        c.points.map((p) => map.getCoordinateFromPixel([p.x, p.y]))
+      );
+      console.log(rings)
+    }
+    return;
+  };
 
   return (
     <MapContext.Provider value={{ map }}>
       <div
         ref={mapElement}
         style={{ height: 'calc(100vh - 60px)', width: '100%' }}
+        onClick={handleClick}
+        onContextMenu={handleClick}
       >
         {project && <ProjectLayer project={project} />}
         {children}
+
+        <Example />
       </div>
     </MapContext.Provider>
   );
