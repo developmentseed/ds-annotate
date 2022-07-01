5ef2e5b418c887cc7bebde3d5db053214f74a68b
Wille Marcel <wille.yyz@gmail.com>
2022-07-01 16:42:12 -0300

    Fix some errors

--- app/components/header.test.js
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import '@testing-library/jest-dom';
-
-import { Header } from './header';
-
-test('render app', () => {
-  const { getByText } = render(<Header />);
-  expect(getByText('ds-annotate')).toBeInTheDocument();
-});
