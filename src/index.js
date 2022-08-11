762bd91a7f2d4f0d0de99ada7c2dbd5dfe90a364
Rub21 <ruben@developmentseed.org>
2022-08-11 14:05:48 -0500

    Update route and deploy

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import MainContextProvider from './contexts/MainContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <MainContextProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/ds-annotate">
         <Routes>
           <Route exact path="/" element={<App />} />
           <Route exact path="/project/:slug" element={<App />} />
@@ -17,8 +16,3 @@ root.render(
       </BrowserRouter>
     </MainContextProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
