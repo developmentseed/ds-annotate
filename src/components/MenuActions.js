9f508f61cd2583a54efa66269fd36ad4015cbf3e
Rub21 <ruben@developmentseed.org>
2022-08-30 10:51:30 -0500

    Update button color

--- src/components/MenuActions.js
+++ src/components/MenuActions.js
@@ -41,7 +41,7 @@ export const MenuActions = () => {
 
       <div className="flex flex-row mt-3">
         <button
-          className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded"
+          className="custom_button"
           onClick={() => {
             merge_polygons();
           }}
