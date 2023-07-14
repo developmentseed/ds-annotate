92c8a2dc792e9bb50e23555c6dde9201a3ca7130
Rub21 <ruben@developmentseed.org>
2023-07-14 17:43:17 -0500

    Fix test

--- src/utils/tests/util.test.js
+++ src/utils/tests/util.test.js
@@ -1,13 +1,13 @@
 import { getProjectTemplate } from "../utils";
 describe("Check query params", () => {
   const url_ =
-    "http://localhost:3000/ds-annotate?classes=farm,00FFFF|tree,FF00FF&name=Farms-mapping&imagery_type=tms&imagery_url=https://gis.apfo.usda.gov/arcgis/rest/services/NAIP/USDA_CONUS_PRIME/ImageServer/tile/{z}/{y}/{x}?blankTile=false&project_bbox=-90.319317,38.482965,-90.247220,38.507418";
+    "http://localhost:3000/ds-annotate?classes=farm,00FFFF|tree,FF00FF&project=Farms-mapping&imagery_type=tms&imagery_url=https://gis.apfo.usda.gov/arcgis/rest/services/NAIP/USDA_CONUS_PRIME/ImageServer/tile/{z}/{y}/{x}?blankTile=false&project_bbox=-90.319317,38.482965,-90.247220,38.507418";
   let url = new URL(url_);
   let params = new URLSearchParams(url.search);
   const projectFeature = getProjectTemplate(params);
 
-  test(`Should return name=${params.get("name")}`, () => {
-    expect(projectFeature.properties.name).toBe(params.get("name"));
+  test(`Should return project=${params.get("project")}`, () => {
+    expect(projectFeature.properties.name).toBe(params.get("project"));
   });
 
   test(`Should return bbox=${params.get("project_bbox")}`, () => {
